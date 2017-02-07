faef5e4545650c798770a54144526663a2790f92
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-02-07 10:25:11 -0600

    Custom fonts

--- ThatCSharpGuy/Views/FeedPage.xaml.cs
+++ ThatCSharpGuy/Views/FeedPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,24 +16,54 @@ namespace ThatCSharpGuy.UI.Views
         public FeedPage()
         {
             InitializeComponent();
+            _store = new PostsStore();
+            _posts = new ObservableCollection<Post>();
+            PostList.ItemsSource = _posts;
+            PostList.ItemSelected += async (sender, e) =>
+            {
+                var selectedPost = PostList.SelectedItem as Post;
+                if (selectedPost != null)
+                {
+                    await Navigation.PushAsync(new PostPage(selectedPost.id));
+                    PostList.SelectedItem = null;
+                }
+            };
 
-			PostList.ItemSelected += async (sender, e) =>
-			{
-				var selectedPost = PostList.SelectedItem as Post;
-				if (selectedPost != null)
-				{
-					await Navigation.PushAsync(new PostPage(selectedPost.id));
-					PostList.SelectedItem = null;
-				}
-			};
+            PostList.ItemAppearing += async (sender, e) =>
+            {
+                if (_posts.Last().Equals(e.Item))
+                {
+                    await LoadData();
+                }
+            };
+        }
+
+        private ObservableCollection<Post> _posts;
+        private int _page = 1;
+        private PostsStore _store;
+
+        async Task LoadData()
+        {
+            var a = new Label();
+            
+            if (_page < 0) return;
+            var response = await _store.GetPosts(_page);
+            foreach (var post in response.Items)
+            {
+                _posts.Add(post);
+            }
+            if (response.CurrentPage == response.TotalPages)
+                _page = -1;
+            else
+            {
+                _page++;
+            }
         }
 
         protected override async void OnAppearing()
         {
             base.OnAppearing();
-            IPostsStore store = new PostsStore();
-			var respns4 = await store.GetPosts(2);
-			PostList.ItemsSource = respns4.Items;
+            await LoadData();
 
         }
     }
